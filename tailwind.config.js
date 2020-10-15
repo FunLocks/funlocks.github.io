a04b379f9068adb55d22e0af2c53eb8123231970
Tomoka Hayashi <g2120042@fun.ac.jp>
2020-10-16 02:32:10 +0900

    first commmit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: ['./*.html'],
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
